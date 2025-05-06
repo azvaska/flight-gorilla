b8f121d7c02093ec7d2c2ac9ac85645ccf469b71
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-05-06 17:45:32 +0200

    feat(frontend): add flexible dates page on flight search

---
+++ frontend/src/app/pages/search/search-dates/price-calendar/price-calendar.component.ts
@@ -0,0 +1,180 @@
+import { BooleanInput, NumberInput } from '@angular/cdk/coercion';
+import {
+  Component,
+  booleanAttribute,
+  computed,
+  input,
+  model,
+  numberAttribute,
+  viewChild,
+} from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideChevronLeft, lucideChevronRight } from '@ng-icons/lucide';
+import {
+  BrnCalendarCellButtonDirective,
+  BrnCalendarCellDirective,
+  BrnCalendarDirective,
+  BrnCalendarGridDirective,
+  BrnCalendarHeaderDirective,
+  BrnCalendarNextButtonDirective,
+  BrnCalendarPreviousButtonDirective,
+  BrnCalendarWeekDirective,
+  BrnCalendarWeekdayDirective,
+  Weekday,
+  injectBrnCalendarI18n,
+} from '@spartan-ng/brain/calendar';
+import { hlm } from '@spartan-ng/brain/core';
+import { injectDateAdapter } from '@spartan-ng/brain/date-time';
+import { buttonVariants } from '@spartan-ng/ui-button-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+import type { ClassValue } from 'clsx';
+import { NgIf, NgClass } from '@angular/common';
+import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+
+@Component({
+  selector: 'price-calendar',
+  imports: [
+    BrnCalendarDirective,
+    BrnCalendarHeaderDirective,
+    BrnCalendarNextButtonDirective,
+    BrnCalendarPreviousButtonDirective,
+    BrnCalendarWeekdayDirective,
+    BrnCalendarWeekDirective,
+    BrnCalendarCellButtonDirective,
+    BrnCalendarCellDirective,
+    BrnCalendarGridDirective,
+    NgIcon,
+    HlmIconDirective,
+    NgIf,
+    NgClass,
+    HlmSpinnerComponent,
+  ],
+  viewProviders: [provideIcons({ lucideChevronLeft, lucideChevronRight })],
+  templateUrl: './price-calendar.component.html',
+})
+export class PriceCalendarComponent<T> {
+  public readonly calendarClass = input<ClassValue>('');
+  protected readonly _computedCalenderClass = computed(() =>
+    hlm('rounded-md border p-3', this.calendarClass())
+  );
+
+  /** Array of strings for each day of the current month */
+  public readonly prices = input<number[]>(
+    Array.from({ length: 31 }, () => Math.floor(Math.random() * 171) + 31)
+  );
+
+  /** Whether the calendar is loading */
+  public readonly isLoading = input<boolean>(false);
+
+  /** Callback to be called when the month changes */
+  public readonly onMonthChangeCallback = input<
+    (month: number, year: number) => void
+  >(() => {});
+
+  /** The minimum date that can be selected.*/
+  public readonly min = input<T>();
+
+  /** The maximum date that can be selected. */
+  public readonly max = input<T>();
+
+  /** Determine if the date picker is disabled. */
+  public readonly disabled = input<boolean, BooleanInput>(false, {
+    transform: booleanAttribute,
+  });
+
+  /** The selected value. */
+  public readonly date = model<T>();
+
+  /** Whether a specific date is disabled. */
+  public readonly dateDisabled = input<(date: T) => boolean>(() => false);
+
+  /** The day the week starts on */
+  public readonly weekStartsOn = input<Weekday, NumberInput>(0, {
+    transform: (v: unknown) => numberAttribute(v) as Weekday,
+  });
+
+  /** The default focused date. */
+  public readonly defaultFocusedDate = input<T>();
+
+  /** Access the calendar directive */
+  private readonly _calendar = viewChild.required(BrnCalendarDirective);
+
+  /** Access the calendar i18n */
+  protected readonly i18n = injectBrnCalendarI18n();
+
+  /** Access the date time adapter */
+  protected readonly dateAdapter = injectDateAdapter<T>();
+
+  /** Get the heading for the current month and year */
+  protected heading = computed(() =>
+    this.i18n.formatHeader(
+      this.dateAdapter.getMonth(this._calendar().focusedDate()),
+      this.dateAdapter.getYear(this._calendar().focusedDate())
+    )
+  );
+
+  protected readonly categorizedPrices = computed<
+    { value: number; type: string }[]
+  >(() => {
+    return this.categorizePrices(this.prices());
+  });
+
+  protected readonly btnClass = hlm(
+    buttonVariants({ variant: 'ghost' }),
+    'h-10 w-10 p-0 font-normal aria-selected:opacity-100',
+    'data-[outside]:text-muted-foreground data-[outside]:opacity-50 data-[outside]:aria-selected:bg-accent/50 data-[outside]:aria-selected:text-muted-foreground data-[outside]:aria-selected:opacity-30',
+    'data-[today]:bg-accent data-[today]:text-accent-foreground',
+    'data-[selected]:bg-primary data-[selected]:text-primary-foreground data-[selected]:hover:bg-primary data-[selected]:hover:text-primary-foreground data-[selected]:focus:bg-primary data-[selected]:focus:text-primary-foreground',
+    'data-[disabled]:text-muted-foreground data-[disabled]:opacity-50'
+  );
+
+  /** Check if a date is in the currently focused month */
+  protected inCurrentMonth(cellDate: T): boolean {
+    return (
+      this.dateAdapter.getMonth(cellDate) ===
+        this.dateAdapter.getMonth(this._calendar().focusedDate()) &&
+      this.dateAdapter.getYear(cellDate) ===
+        this.dateAdapter.getYear(this._calendar().focusedDate())
+    );
+  }
+
+  protected categorizePrices(prices: number[]) {
+    const mean = prices.reduce((a, b) => a + b, 0) / prices.length;
+    const stdDev = Math.sqrt(
+      prices.reduce((sum, price) => sum + Math.pow(price - mean, 2), 0) /
+        prices.length
+    );
+    const margin = 0.5 * stdDev;
+
+    return prices.map((price) => {
+      let type;
+      if (price < mean - margin) {
+        type = 'underpriced';
+      } else if (price > mean + margin) {
+        type = 'overpriced';
+      } else {
+        type = 'average';
+      }
+      return { value: price, type };
+    });
+  }
+
+  /** Get the string entry for a given date */
+  protected getPriceForDate(cellDate: T): string {
+    if (!this.inCurrentMonth(cellDate)) {
+      return '';
+    }
+    const dayIndex = this.dateAdapter.getDate(cellDate) - 1;
+    return this.prices()[dayIndex] ? this.prices()[dayIndex] + '€' : '';
+  }
+
+  protected getPriceTypeForDate(cellDate: T): string {
+    const dayIndex = this.dateAdapter.getDate(cellDate) - 1;
+    return this.categorizedPrices()[dayIndex]?.type || '';
+  }
+
+  protected onMonthChange() {
+    const currentDate = new Date(this._calendar().focusedDate());
+    this.onMonthChangeCallback()(currentDate.getMonth() + 1, currentDate.getFullYear());
+  }
+}
--- frontend/src/app/pages/search/search-dates/search-dates.component.ts
+++ frontend/src/app/pages/search/search-dates/search-dates.component.ts
@@ -1,28 +1,62 @@
 import { Component } from '@angular/core';
 import {
   HlmCardContentDirective,
-  HlmCardDescriptionDirective,
   HlmCardDirective,
-  HlmCardFooterDirective,
   HlmCardHeaderDirective,
   HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
-import { HlmCalendarComponent } from '@spartan-ng/ui-calendar-helm';
+import { PriceCalendarComponent } from './price-calendar/price-calendar.component';
 
 @Component({
   selector: 'app-search-dates',
   imports: [
-    // HlmCardDirective,
-    // HlmCardHeaderDirective,
-    // HlmCardTitleDirective,
-    // HlmCardDescriptionDirective,
-    // HlmCardContentDirective,
-    // HlmCardFooterDirective,
-    // HlmCalendarComponent,
+    HlmCardDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+    HlmCardContentDirective,
+    PriceCalendarComponent,
   ],
   templateUrl: './search-dates.component.html',
   host: {
     class: 'block w-full h-full',
   },
 })
-export class SearchDatesComponent {}
+export class SearchDatesComponent {
+  protected departureMinDate = new Date();
+  protected returnMinDate = new Date();
+  protected maxDate = new Date(
+    new Date().setFullYear(new Date().getFullYear() + 1)
+  );
+
+  protected departureDate: Date | undefined = undefined;
+  protected returnDate: Date | undefined = undefined;
+
+  protected departureLoading = false;
+  protected returnLoading = false;
+
+  protected onDepartureDateChange(date: Date) {
+    this.departureDate = date;
+    this.returnMinDate = date;
+    this.returnDate = undefined;
+  }
+
+  protected onReturnDateChange(date: Date) {
+    this.returnDate = date;
+  }
+
+  protected onDepartureMonthChange(month: number, year: number) {
+    // TODO: Placehodler
+    this.departureLoading = true;
+    setTimeout(() => {
+      this.departureLoading = false;
+    }, 2000);
+  }
+
+  protected onReturnMonthChange(month: number, year: number) {
+    // TODO: Placehodler
+    this.returnLoading = true;
+    setTimeout(() => {
+      this.returnLoading = false;
+    }, 2000);
+  }
+}
