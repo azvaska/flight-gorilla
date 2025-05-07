72b7b31bc6281964f785917e022c68abf0315c70
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-05-07 17:24:39 +0200

    feat(frontend): add barebones flight search page

--- frontend/src/app/pages/search/search-flights/search-flights.component.ts
+++ frontend/src/app/pages/search/search-flights/search-flights.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FlightCardComponent } from './components/flight-card/flight-card.component';
+import {
+  HlmCardDirective,
+  HlmCardContentDirective,
+} from '@spartan-ng/ui-card-helm';
 @Component({
   selector: 'app-search-flights',
-  imports: [],
+  imports: [FlightCardComponent, HlmCardDirective, HlmCardContentDirective],
   templateUrl: './search-flights.component.html'
 })
 export class SearchFlightsComponent {
