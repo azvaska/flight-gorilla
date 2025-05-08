7b3790678e06ce061e699f802f17424b03c53708
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-05-08 16:54:32 +0200

    feat(frontend): minor style changes

--- frontend/src/app/pages/search/search-flights/search-flights.component.ts
+++ frontend/src/app/pages/search/search-flights/search-flights.component.ts
@@ -4,9 +4,10 @@ import {
   HlmCardDirective,
   HlmCardContentDirective,
 } from '@spartan-ng/ui-card-helm';
+import { BookingComponent } from "../../booking/booking.component";
 @Component({
   selector: 'app-search-flights',
-  imports: [FlightCardComponent, HlmCardDirective, HlmCardContentDirective],
+  imports: [FlightCardComponent, HlmCardDirective, HlmCardContentDirective, BookingComponent],
   templateUrl: './search-flights.component.html'
 })
 export class SearchFlightsComponent {
