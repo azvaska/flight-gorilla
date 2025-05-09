508c556710fa460478da4b1dbf7f682b5d75101c
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-05-09 17:23:15 +0200

    feat(frontend): add auth UI

---
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,50 @@
+// login.component.ts
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  HlmCardDirective,
+  HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+  HlmCardDescriptionDirective,
+  HlmCardContentDirective,
+  HlmCardFooterDirective,
+} from '@spartan-ng/ui-card-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    HlmCardDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+    HlmCardDescriptionDirective,
+    HlmCardContentDirective,
+    HlmCardFooterDirective,
+    HlmLabelDirective,
+    HlmInputDirective,
+    HlmButtonDirective,
+  ],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    remember: new FormControl(false),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // handle login
+    }
+  }
+}
