3142d21d0e030bf22c5fd96d484e150abe6223bf
PaesaggioSonoro <francesco.zanusfortes@gmail.com>
2025-05-18 12:23:40 +0200

    feat: my-reservations graphical enhancements

--- frontend/src/app/pages/reservations/my-reservations/my-reservations.component.ts
+++ frontend/src/app/pages/reservations/my-reservations/my-reservations.component.ts
@@ -1,20 +1,23 @@
 // src/app/my-reservations/my-reservations.component.ts
 import { Component } from '@angular/core';
-import {NgForOf, NgIf, SlicePipe} from '@angular/common';
+import {NgForOf, NgIf, NgOptimizedImage, NgTemplateOutlet, SlicePipe} from '@angular/common';
 import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
 import {ActivatedRoute, Router} from '@angular/router';
 
 interface FlightSegment {
   departureAirport: string;
   arrivalAirport:   string;
+  departureDate:    string; // e.g. "11 Jul 2025"
   departureTime:    string; // e.g. "08:00"
+  arrivalDate:      string; // e.g. "11 Jul 2025"
   arrivalTime:      string; // e.g. "10:00"
 }
 
 interface Reservation {
   id: number;
-  name: string; // e.g. "Venice to Dublin"
-  segments: FlightSegment[];  // [0] = outbound, [1] = return (if any)
+  name: string;
+  departure: FlightSegment[];
+  return:   FlightSegment[];
 }
 
 @Component({
@@ -22,8 +25,9 @@ interface Reservation {
   imports: [
     NgIf,
     NgForOf,
-    SlicePipe,
-    HlmButtonDirective
+    HlmButtonDirective,
+    NgTemplateOutlet,
+    NgOptimizedImage
   ],
   host: {
     class: 'block w-full h-full',
@@ -35,17 +39,42 @@ export class MyReservationsComponent {
     {
       id: 1,
       name: 'Venice to Dublin',
-      segments: [
-        { departureAirport: 'VCE', arrivalAirport: 'CDG', departureTime: '08:00', arrivalTime: '10:00' },
-        { departureAirport: 'CDG', arrivalAirport: 'DUB', departureTime: '12:00', arrivalTime: '13:30' }
+      departure: [
+        {
+          departureAirport: 'VCE', arrivalAirport: 'CDG',
+          departureDate: '11 Jul 2025', departureTime: '08:00',
+          arrivalDate:   '11 Jul 2025', arrivalTime:   '10:00'
+        },
+        {
+          departureAirport: 'CDG', arrivalAirport: 'DUB',
+          departureDate: '12 Jul 2025', departureTime: '12:00',
+          arrivalDate:   '12 Jul 2025', arrivalTime:   '13:30'
+        },
+      ],
+      return: [
+        {
+          departureAirport: 'DUB', arrivalAirport: 'CDG',
+          departureDate: '18 Jul 2025', departureTime: '09:00',
+          arrivalDate:   '18 Jul 2025', arrivalTime:   '10:00'
+        },
+        {
+          departureAirport: 'CDG', arrivalAirport: 'VCE',
+          departureDate: '18 Jul 2025', departureTime: '15:00',
+          arrivalDate:   '18 Jul 2025', arrivalTime:   '19:30'
+        },
       ]
     },
     {
       id: 2,
       name: 'London to New York',
-      segments: [
-        { departureAirport: 'LHR', arrivalAirport: 'JFK', departureTime: '09:00', arrivalTime: '12:30' }
-      ]
+      departure: [
+        {
+          departureAirport: 'LHR', arrivalAirport: 'JFK',
+          departureDate: '20 Jul 2025', departureTime: '09:00',
+          arrivalDate:   '20 Jul 2025', arrivalTime:   '12:30'
+        }
+      ],
+      return: []
     }
   ];
 
