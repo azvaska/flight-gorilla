9d579a0b9dc0b0b205536995a4916ec414e9e3fb
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-06-07 16:53:31 +0200

    fix: change aircraft list table styling

--- frontend/src/app/pages/airline/aircraft-list/aircraft-list.component.ts
+++ frontend/src/app/pages/airline/aircraft-list/aircraft-list.component.ts
@@ -3,16 +3,35 @@ import {NgForOf, NgOptimizedImage} from '@angular/common';
 import { AirlineAircraft } from '@/types/airline/airlineAircraft';
 import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
 import {RouterLink} from '@angular/router';
-
+import {
+  HlmCaptionComponent,
+  HlmTableComponent,
+  HlmThComponent,
+  HlmTrowComponent,
+} from '@spartan-ng/ui-table-helm';
+import {
+  HlmCardDirective,
+} from '@spartan-ng/ui-card-helm';
+import { lucideEllipsis } from '@ng-icons/lucide';
+import { provideIcons, NgIcon } from '@ng-icons/core';
+import { PopoverComponent } from '@/app/components/ui/popover/popover.component';
+import { PopoverTriggerDirective } from '@/app/components/ui/popover/popover-trigger.directive';
 
 @Component({
   selector: 'app-aircrafts-list',
   templateUrl: './aircraft-list.component.html',
+  providers: [provideIcons({ lucideEllipsis })],
   imports: [
     NgForOf,
-    NgOptimizedImage,
     HlmButtonDirective,
-    RouterLink
+    RouterLink,
+    HlmTableComponent,
+    HlmTrowComponent,
+    HlmThComponent,
+    HlmCardDirective,
+    NgIcon,
+    PopoverComponent,
+    PopoverTriggerDirective,
   ],
   host: {
     class: 'block w-full h-fit',
@@ -23,6 +42,42 @@ export class AircraftListComponent {
   aircrafts: AirlineAircraft[] = [
     { id: '0', model: 'Boeing 737', tailNumber: 'N12345', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
     { id: '1', model: 'Airbus A320', tailNumber: 'F-GKXB', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
+    { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
     { id: '2', model: 'Cessna 172', tailNumber: 'G-ABCD', firstClassSeats: ["1A"], businessClassSeats: ["1B"], economyClassSeats: ["1C"]},
   ];
 
